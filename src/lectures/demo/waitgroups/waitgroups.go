95b08da7a2763099e5236a386a4b55d5df3e9e03
Bhavin1996 <bhavin.bawane@gmail.com>
2023-09-06 11:11:46 +0530

    Update waitgroups.go
    In demo this was updated

--- src/lectures/demo/waitgroups/waitgroups.go
+++ src/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	var wg sync.WaitGroup
+	counter := 0
+	var input int
+	fmt.Scan(&input)
+	for i := 0; i < input; i++ {
+		wg.Add(1)
+		counter += 1
+		go func() {
+			defer func() {
+				fmt.Println(counter, "gouritines remaining")
+				counter -= 1
+				wg.Done()
+			}()
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Println("Waiting for duration", duration)
+			time.Sleep(duration)
+		}()
+	}
+	fmt.Println("Running goroutines")
+	wg.Wait()
+	fmt.Println("Program exited")
 }
